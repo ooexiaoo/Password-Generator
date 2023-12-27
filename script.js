455372a1cd533a8a960c6673c626cf3f38729889
ooexiaoo <vpaherwar@gmail.com>
2023-12-28 03:32:07 +0530

    logic when everything is unchecked

--- script.js
+++ script.js
@@ -1,63 +1,54 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const lengthRange = document.getElementById('lengthRange');
-    const lengthValue = document.getElementById('lengthValue');
-    const uppercaseCheck = document.getElementById('uppercaseCheck');
-    const lowercaseCheck = document.getElementById('lowercaseCheck');
-    const numbersCheck = document.getElementById('numbersCheck');
-    const specialCharsCheck = document.getElementById('specialCharsCheck');
-    const generateBtn = document.getElementById('generateBtn');
-    const passwordOutput = document.getElementById('passwordOutput');
-    const copyBtn = document.getElementById('copyBtn');
-  
-    lengthRange.addEventListener('input', function () {
-      lengthValue.textContent = lengthRange.value;
-      generatePassword();
-      changeBackgroundColor();
-    });
-  
-    generateBtn.addEventListener('click', function () {
-      generatePassword();
-      changeBackgroundColor();
-    });
-  
-    copyBtn.addEventListener('click', function () {
-      passwordOutput.select();
-      document.execCommand('copy');
-      alert('Password copied to clipboard!');
-    });
-  
-    uppercaseCheck.addEventListener('change', function () {
-      generatePassword();
-    });
-  
-    lowercaseCheck.addEventListener('change', function () {
-      generatePassword();
-    });
-  
-    numbersCheck.addEventListener('change', function () {
-      generatePassword();
-    });
-  
-    specialCharsCheck.addEventListener('change', function () {
-      generatePassword();
-    });
-  
-    function generatePassword() {
-      const length = lengthRange.value;
-      const uppercase = uppercaseCheck.checked;
-      const lowercase = lowercaseCheck.checked;
-      const numbers = numbersCheck.checked;
-      const specialChars = specialCharsCheck.checked;
-      const charset = generateCharset(uppercase, lowercase, numbers, specialChars);
-  
-      let password = '';
-      for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * charset.length);
-        password += charset[randomIndex];
-      }
-  
-      passwordOutput.value = password;
+  const lengthRange = document.getElementById('lengthRange');
+  const lengthValue = document.getElementById('lengthValue');
+  const uppercaseCheck = document.getElementById('uppercaseCheck');
+  const lowercaseCheck = document.getElementById('lowercaseCheck');
+  const numbersCheck = document.getElementById('numbersCheck');
+  const specialCharsCheck = document.getElementById('specialCharsCheck');
+  const generateBtn = document.getElementById('generateBtn');
+  const passwordOutput = document.getElementById('passwordOutput');
+  const copyBtn = document.getElementById('copyBtn');
+
+  lengthRange.addEventListener('input', function () {
+    lengthValue.textContent = lengthRange.value;
+    generatePassword();
+    changeBackgroundColor();
+  });
+
+  generateBtn.addEventListener('click', function () {
+    if (!(uppercaseCheck.checked || lowercaseCheck.checked || numbersCheck.checked || specialCharsCheck.checked)) {
+      alert('Please select at least one option.');
+      return;
     }
+
+    generatePassword();
+    changeBackgroundColor();
+  });
+
+  copyBtn.addEventListener('click', function () {
+    passwordOutput.select();
+    document.execCommand('copy');
+    alert('Password copied to clipboard!');
+  });
+
+  // Event listeners for checkbox changes...
+
+  function generatePassword() {
+    const length = lengthRange.value;
+    const uppercase = uppercaseCheck.checked;
+    const lowercase = lowercaseCheck.checked;
+    const numbers = numbersCheck.checked;
+    const specialChars = specialCharsCheck.checked;
+    const charset = generateCharset(uppercase, lowercase, numbers, specialChars);
+
+    let password = '';
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * charset.length);
+      password += charset[randomIndex];
+    }
+
+    passwordOutput.value = password;
+  }
   
     function generateCharset(uppercase, lowercase, numbers, specialChars) {
       let charset = '';
@@ -66,7 +57,7 @@ document.addEventListener('DOMContentLoaded', function () {
       if (numbers) charset += '0123456789';
       if (specialChars) charset += '!@#$%^&*()_+-=[]{}|;:,.<>?';
   
-      return charset;
+      return charset || ' ';
     }
   
     function changeBackgroundColor() {
