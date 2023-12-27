215f5697443d44aaa3ae3f4d2230d8a42388a01e
ooexiaoo <vpaherwar@gmail.com>
2023-12-27 21:00:00 +0530

    change full UI, added slider, added color changing with password length

--- script.js
+++ script.js
@@ -1,33 +1,98 @@
-function generatePassword() {
-    const length = document.getElementById('length').value;
-    const uppercase = document.getElementById('uppercase').checked;
-    const lowercase = document.getElementById('lowercase').checked;
-    const numbers = document.getElementById('numbers').checked;
-    const special = document.getElementById('special').checked;
+document.addEventListener('DOMContentLoaded', function () {
+    const lengthRange = document.getElementById('lengthRange');
+    const lengthValue = document.getElementById('lengthValue');
+    const uppercaseCheck = document.getElementById('uppercaseCheck');
+    const lowercaseCheck = document.getElementById('lowercaseCheck');
+    const numbersCheck = document.getElementById('numbersCheck');
+    const specialCharsCheck = document.getElementById('specialCharsCheck');
+    const generateBtn = document.getElementById('generateBtn');
+    const passwordOutput = document.getElementById('passwordOutput');
+    const copyBtn = document.getElementById('copyBtn');
   
-    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
-    const numberChars = '0123456789';
-    const specialChars = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+    lengthRange.addEventListener('input', function () {
+      lengthValue.textContent = lengthRange.value;
+      generatePassword();
+      changeBackgroundColor();
+    });
   
-    let charset = '';
-    if (uppercase) charset += uppercaseChars;
-    if (lowercase) charset += lowercaseChars;
-    if (numbers) charset += numberChars;
-    if (special) charset += specialChars;
+    generateBtn.addEventListener('click', function () {
+      generatePassword();
+      changeBackgroundColor();
+    });
   
-    let password = '';
-    for (let i = 0; i < length; i++) {
-      const randomIndex = Math.floor(Math.random() * charset.length);
-      password += charset[randomIndex];
+    copyBtn.addEventListener('click', function () {
+      passwordOutput.select();
+      document.execCommand('copy');
+      alert('Password copied to clipboard!');
+    });
+  
+    uppercaseCheck.addEventListener('change', function () {
+      generatePassword();
+    });
+  
+    lowercaseCheck.addEventListener('change', function () {
+      generatePassword();
+    });
+  
+    numbersCheck.addEventListener('change', function () {
+      generatePassword();
+    });
+  
+    specialCharsCheck.addEventListener('change', function () {
+      generatePassword();
+    });
+  
+    function generatePassword() {
+      const length = lengthRange.value;
+      const uppercase = uppercaseCheck.checked;
+      const lowercase = lowercaseCheck.checked;
+      const numbers = numbersCheck.checked;
+      const specialChars = specialCharsCheck.checked;
+      const charset = generateCharset(uppercase, lowercase, numbers, specialChars);
+  
+      let password = '';
+      for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+      }
+  
+      passwordOutput.value = password;
     }
   
-    document.getElementById('password').value = password;
-  }
+    function generateCharset(uppercase, lowercase, numbers, specialChars) {
+      let charset = '';
+      if (uppercase) charset += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      if (lowercase) charset += 'abcdefghijklmnopqrstuvwxyz';
+      if (numbers) charset += '0123456789';
+      if (specialChars) charset += '!@#$%^&*()_+-=[]{}|;:,.<>?';
   
-  function copyPassword() {
-    const passwordField = document.getElementById('password');
-    passwordField.select();
-    document.execCommand('copy');
-    alert('Password copied to clipboard!');
-  }
+      return charset;
+    }
+  
+    function changeBackgroundColor() {
+      const passwordStrength = getPasswordStrength();
+      let backgroundColor;
+      if (passwordStrength === 'weak') {
+        backgroundColor = '#ff9999'; // Red for weak passwords
+      } else if (passwordStrength === 'medium') {
+        backgroundColor = '#ffff99'; // Yellow for medium passwords
+      } else {
+        backgroundColor = '#99ff99'; // Green for strong passwords
+      }
+      document.body.style.transition = 'background-color 0.5s ease-in-out';
+      document.body.style.backgroundColor = backgroundColor;
+    }
+  
+    function getPasswordStrength() {
+      const password = passwordOutput.value;
+      const length = password.length;
+      if (length < 12) {
+        return 'weak';
+      } else if (length < 94) {
+        return 'medium';
+      } else {
+        return 'strong';
+      }
+    }
+  });
+  
