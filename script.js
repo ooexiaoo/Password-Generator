5fe6a256d69afb770e0f11832318dba29031784a
ooexiaoo <vpaherwar@gmail.com>
2023-12-27 15:53:55 +0530

    27-12-2023 - 03:53pm first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function generatePassword() {
+    const length = document.getElementById('length').value;
+    const uppercase = document.getElementById('uppercase').checked;
+    const lowercase = document.getElementById('lowercase').checked;
+    const numbers = document.getElementById('numbers').checked;
+    const special = document.getElementById('special').checked;
+  
+    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const numberChars = '0123456789';
+    const specialChars = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+  
+    let charset = '';
+    if (uppercase) charset += uppercaseChars;
+    if (lowercase) charset += lowercaseChars;
+    if (numbers) charset += numberChars;
+    if (special) charset += specialChars;
+  
+    let password = '';
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * charset.length);
+      password += charset[randomIndex];
+    }
+  
+    document.getElementById('password').value = password;
+  }
+  
